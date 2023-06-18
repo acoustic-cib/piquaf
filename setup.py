6327d83df41b7bdda4280ecf8158c8a66fdd1c6f
Cibby Pulikkaseril <cibby.pulikkaseril@baraja.com>
2023-06-18 22:01:38 +1000

    added setup and init for package. Good progress on notebook

---
+++ setup.py
@@ -0,0 +1,14 @@
+""" Install file for piquaf """
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="piquaf",
+    version="0.1",
+    description="Create Publication Quality Figures from matplotlib figures",
+    author="Cibby Pulikkaseril",
+    author_email="",
+    url="https://github.com/acoustic-cib/piquaf",
+    packages=["piquaf"],
+)

7823cdf4f735f91f5138c1b712bffa4a6dc23661
Cibby Pulikkaseril <cibby.pulikkaseril@baraja.com>
2023-06-20 21:51:31 +1000

    data files created in plt2pyx, notebook is working to create PDFs

--- piquaf/plt2pyx.py
+++ piquaf/plt2pyx.py
@@ -10,8 +10,11 @@ plt2gle.py - top level functions
 
 """
 import tempfile
-from piquaf.extools import get_title, get_xlabel, get_ylabel, extract_data
+import os
 from pyx import graph
+from piquaf.extools import get_title, get_xlabel, get_ylabel, extract_data
+from numpy import savetxt, column_stack, array
+
 
 def createLine2D(fig):
     """
@@ -20,17 +23,28 @@ def createLine2D(fig):
     outpath = createLine2D(fig)
 
     Give a figure handle in fig, this function will
-    extract the relevant information and create the 
-    PyX figure and 
+    extract the relevant information and create the
+    PyX figure and
     """
     title = get_title(fig)
     xlabel = get_xlabel(fig)
     ylabel = get_ylabel(fig)
     list_line = extract_data(fig)
-    
-	# create Line2D graph
+
+    ###########################
+    # create Line2D graph
+    ###########################
+    cur_dir = os.path.curdir
+    # first create data files for each line in list_line
     with tempfile.TemporaryDirectory() as tmpdirname:
-        
+        print(tmpdirname)
+
+        for ind, this_line in enumerate(list_line):
+            x = array(this_line["x"])
+            y = array(this_line["y"])
+
+            save_path = os.path.join(cur_dir, f"{ind:d}.dat")
+            savetxt(save_path, column_stack([x, y]), fmt="%.4f", delimiter=" ")
 
 
 """
